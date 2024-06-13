4a4cffae38d3c5b743ff0c50b1a873696ac86dc6
MaFe1401 <marcferre42@gmail.com>
2024-06-13 14:56:29 +0200

    api minor changes

--- sshServer/channelSingleton.py
+++
@@ -1,5 +0,0 @@
-class ChannelSingleton(object):
-    def __new__(cls):
-        if not hasattr(cls,'instance'):
-            cls.instance=super(ChannelSingleton,cls).__new__(cls)
-        return cls.instance
