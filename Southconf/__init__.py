175b76dac8996011d18ea31a359cec55e6ce3acd
MaFe1401 <marcferre42@gmail.com>
2024-07-18 15:27:21 +0200

    streams detection implemented

--- Southconf/__init__.py
+++ Southconf/__init__.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
         config = getconfig()
         #config = """<data xmlns="urn:ietf:params:xml:ns:netconf:base:1.0"><keystore xmlns="urn:ietf:params:xml:ns:yang:ietf-keystore"><asymmetric-keys><asymmetric-key><name>genkey</name><algorithm>rsa2048</algorithm><public-key>MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsKs9JB7+4mXV0qmEmmE8r3bU2sifGKrtwYai1kLqbFEVp5EicfF1LFLcEjr04hIjTwi/FDkGmli9zizsiqpXQj/bmX0k3f+Oztu7Ozk5tdGyrC095k5Ntfm8y7JT4Fp7caxU2Sc6RtByI1H9FjWGcCBNx2PG2QGHXQ4t/JEttKEawY3VmwtF1MZsglQ4fKLnHoDi4R49dsPWuYgUhCqLMdE+t5miFa/DNCFOx52kNbKffzgSZsoCOa/hBVoe2qNu/Vvb/6R+ZjEh2Z2CBbYVqc6TtCTKJLElyJLTQRZHwFY2a//jbzY+7avF3epSCx61gXXqmN5I0x+wYjKDYy2tDQIDAQAB</public-key></asymmetric-key></asymmetric-keys></keystore><netconf-server xmlns="urn:ietf:params:xml:ns:yang:ietf-netconf-server"><listen><endpoint><name>default-ssh</name><ssh><tcp-server-parameters><local-address>0.0.0.0</local-address><keepalives><idle-time>1</idle-time><max-probes>10</max-probes><probe-interval>5</probe-interval></keepalives></tcp-server-parameters><ssh-server-parameters><server-identity><host-key><name>default-key</name><public-key><keystore-reference>genkey</keystore-reference></public-key></host-key></server-identity><client-authentication><supported-authentication-methods><publickey/><passsword/></supported-authentication-methods></client-authentication></ssh-server-parameters></ssh></endpoint></listen></netconf-server></data>"""
         if (config not in currentConfig):
-            print("----NEW CONFIG----")
+            print("----NEW TAS CONFIG DETECTED----")
             currentConfig.clear()
-            print(config)
+            #print(config)
             currentConfig.append(config)
             print("----Waiting for new config----") 
             try:
@@ -22,13 +22,13 @@ if __name__ == "__main__":
                 print(interfaces)
             except Exception as e:
                 print("No TAS configuration found yet")
-                print (e)
+                #print (e)
                 continue
             print("---------------------------------------")
-            print("---- DS-TT TAS CONFIGURATION ----")
+            print("---- DS-TT TAS CONFIGURATION EDITED----")
             dsttConfig = prepareDstt(interfaces)
             editconfig(dsttIp, dsttConfig)
-            print("---- NW-TT TAS CONFIGURATION ----")
+            print("---- NW-TT TAS CONFIGURATION EDITED----")
             nwttConfig = prepareNwtt(interfaces)
             editconfig(nwttIp, nwttConfig)
-        
+            countStreams(dsttConfig)
